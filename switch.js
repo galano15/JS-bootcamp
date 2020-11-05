8e139d9348d41dd6cd9b5e0a5abb8b9e81965ef1
galano15 <73849309+galano15@users.noreply.github.com>
2020-11-05 21:53:01 +0200

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,22 @@
+function fruits(fruittype){
+switch (fruittype){
+
+        case "Bananas":
+            console.log("0.99")
+            break;
+        case "Orangers":
+            console.log("0,79")
+            break;
+        case "Apples":
+            console.log("0,89")
+            break;
+        case "Cherries":
+            console.log("2,99")
+            break;
+        case "Grapes":
+            console.log("3,99")
+            break;
+        default:console.log("Wrong Fruits")
+            break;            
+}
+}
